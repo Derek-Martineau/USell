6a86c6f7c844bbca93f49935ea7c3e9aec6f9ec3
Derek Martineau <derekmartineau@hotmail.com>
2024-02-05 19:46:24 -0500

    backend(feat): Delete One Product

--- backend/server/routes/productDelete.js
+++ backend/server/routes/productDelete.js
@@ -2,9 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Product = require('../models/productModel'); 
 
+//Delete All Products
 router.post('/deleteAll', async (req, res) => {
     const products = await Product.deleteMany();
     return res.json(products)
+});
+
+//Delete All Product
+router.delete('/deleteOne/:productId', async (req, res) => {
+    try {
+        const products = await Product.findByIdAndRemove(req.params.productId);
+        if (products) {
+            res.status(204).end;
+        } else {
+            res.status(404).json({ message: 'Result not found' }); //productId not found
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
 });
 
 module.exports = router;
